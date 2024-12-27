e73c74e63ff4b3ba4c6b1ef63bdc29a16572250f
Your Name <you@example.com>
2024-12-27 22:12:07 +0000

    Added code to create a function displaying the program title

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+This module controls the overall program flow for the Disneyland Reviews project.
+It serves as the entry point for the application, displaying the main menu
+and calling relevant functions from other modules based on user input.
+
+Author: Joao Victor Garcia Schiavo
+Date: 27/12/2024
+"""
+
+
+def main():
+    """
+        Main function that runs the Disneyland Reviews program.
+
+        Displays the main menu, handles user input, and directs the program
+        flow to sub-menus or exits the program.
+        """
+    # Program title
+    title = "Disneyland Park Review"
+    print("-" * len(title))
+    print(title)
+    print("-" * len(title))
+
