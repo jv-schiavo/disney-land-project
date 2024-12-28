fe936752bd3cb60b97fae646ae1ef443d406c242
Your Name <you@example.com>
2024-12-28 22:00:03 +0000

    Added new function to display all reviews based on user's choice

--- tui.py
+++ tui.py
@@ -5,10 +5,12 @@ It handles all user interactions, including displaying menus and getting user in
 Author: Joao Victor Garcia Schiavo
 Date: 27/12/2014
 """
+from fontTools.feaLib.variableScalar import Location
+
 
 def menu_choice():
     # Display the main menu and accepts user input
-    print("Please enter the letter which corresponds with your desired menu choice:")
+    print("\nPlease enter the letter which corresponds with your desired menu choice:")
     print("\n[A] View Data")
     print("[B] Visualize Data")
     print("[X] Exit")
@@ -32,3 +34,18 @@ def visualize_data():
     print("[C] Park Ranking by Nationality")
     print("[D] Most Popular Month by Park")
     return input().strip().upper() #str: the user's sub-menu choice
+
+def reviews_park(data):
+    # Display all reviews for a specific park based on user input
+    park_name = input("\nPlease enter the name park you wish to see reviews for: (Eg: Disneyland_HongKong, Disneyland_California)")
+    # Filter reviews for the specified park
+    filtered_reviews = [row for row in data if row['Branch'] == park_name]
+
+    if not filtered_reviews:
+        print(f"No reviews found for {park_name}")
+        return
+
+    print(f"\nReviews for {park_name}:")
+    for review in filtered_reviews:
+        print(f"Reviews ID: {review['Review ID']}, Rating: {review['Rating']},"
+              f"Date: {review['Year_Month']}, Location: {review['Reviewer_Location']}")
