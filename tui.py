3938f49303b28ed0af46b748449043c4605a1f8f
Your Name <you@example.com>
2024-12-29 18:14:37 +0000

    Created function to display the average rating for the given park in the given year

--- tui.py
+++ tui.py
@@ -34,8 +34,11 @@ def visualize_data():
     return input().strip().upper() #str: the user's sub-menu choice
 
 def reviews_park(data):
+
     # Display all reviews for a specific park based on user input
-    park_name = input("\nPlease enter the name park you wish to see reviews for: (Eg: Disneyland_HongKong, Disneyland_California)")
+    park_name = input("\nPlease enter the name park you wish to see reviews for: (Eg: Disneyland_HongKong, Disneyland_"
+                      "California, Disneyland_Paris)\n")
+
     # Filter reviews for the specified park
     filtered_reviews = [row for row in data if row['Branch'] == park_name]
 
@@ -49,21 +52,57 @@ def reviews_park(data):
               f"Date: {review['Year_Month']}, Location: {review['Reviewer_Location']}")
 
 def rev_park_location(data):
+
     # Display the number of reviews a specific park has received from a given location
-    park_name = input("\nPlease enter the name park you wish to see reviews for: (Eg: Disneyland_HongKong, Disneyland_California, Disneyland_Paris)\n")
+    park_name = input("\nPlease enter the name park you wish to see reviews for: (Eg: Disneyland_HongKong, Disneyland_"
+                      "California, Disneyland_Paris)\n")
+
     # Filter reviews for the specified park
     filtered_reviews = [row for row in data if row['Branch'] == park_name]
 
     if not filtered_reviews:
         print(f"No reviews found for {park_name}")
         return
+
     # Filter reviews for the specified reviewer location
     reviewer_location = input("\nPlease enter the reviewer location you wish to see reviews for: \n")
 
-    filtered_location = [row for row in data if row['Reviewer_Location'] == reviewer_location]
+    filtered_location = [row for row in filtered_reviews if row['Reviewer_Location'] == reviewer_location]
 
     if not filtered_location:
         print(f"No reviews found for {park_name}")
         return
     # Display the number of reviews
     print(f"\nNumber of reviews for {park_name} from {reviewer_location}: {len(filtered_location)}")
+
+def avg_rating(data):
+
+    # Display the average rating for the given park in the given year
+    park_name = input("\nPlease enter the name park you wish to see reviews for: (Eg: Disneyland_HongKong, Disneyland_"
+                      "California, Disneyland_Paris)\n")
+
+    # Filter reviews for the specified park
+    filtered_reviews = [row for row in data if row['Branch'] == park_name]
+    if not filtered_reviews:
+        print(f"No reviews found for {park_name}")
+        return
+
+    year = input("\nPlease enter the year you wish to see reviews for: \n")
+
+    # Filter reviews for specified year
+    filtered_date = [row for row in filtered_reviews if row['Year_Month'][:4] == year]
+    if not filtered_date:
+        print(f"No reviews found for {year}")
+        return
+
+    # Extract ratings
+    ratings = [int(row['Rating']) for row in filtered_date if 'Rating' in row and row['Rating'].isdigit()]
+    if not ratings:
+        print("No valid ratings found in the dataset.")
+        return None
+
+    #Calculate the average
+    average = sum(ratings) / len(ratings)
+
+    #Display the average rating
+    print(f"\n The average rating score for {park_name} in {year} is {average:.2f}")
