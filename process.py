6bc1d1a9ae43a148f2deaea7b2a166dbb9ab28c2
joao victor schiavo <jvgs.schiavo@gmail.com>
2025-03-26 19:26:08 +0000

    Amend code so dataset is not hardcoded to my local machine

--- process.py
+++ process.py
@@ -2,25 +2,30 @@
 This module is responsible for processing the data.  It will largely contain functions that will receive the overall dataset and
 perform necessary processes in order to provide the desired result in the desired format.
 It is likely that most sections will require functions to be placed in this module.
+
+Author: Joao Victor Garcia Schiavo
+Date: 31/12/2024
 """
 
 
 import csv
+import os
 
-def load_csv(file_path):
 
+file_path = os.path.join(os.path.dirname(__file__), 'data', 'disneyland_reviews.csv')
+
+def load_csv(path=file_path):
     data = []
     try:
-        with open ('C:\\Users\\Joao Victor\\Downloads\\project_template\\data\\disneyland_reviews.csv', mode='r') as file:
-            # Read CSV as dictionaries
+        with open(path, mode='r', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 data.append(row)
         print(f"\nSuccessfully loaded {len(data)} rows")
     except FileNotFoundError:
-        print("File not found")
+        print(f"File not found at path: {path}")
     return data
 
+
 if __name__ == "__main__":
-    file_path = r'C:\Users\Joao Victor\Downloads\project_template\data\disneyland_reviews.csv'
     data = load_csv(file_path)
